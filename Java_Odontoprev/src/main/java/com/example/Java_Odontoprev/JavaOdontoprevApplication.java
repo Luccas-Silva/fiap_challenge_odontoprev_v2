a7770ae58f08db047b8fe060377ed7333518dc0f
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-03-09 17:18:53 -0300

    Starting Java

---
+++ Java_Odontoprev/src/main/java/com/example/Java_Odontoprev/JavaOdontoprevApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java_Odontoprev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaOdontoprevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaOdontoprevApplication.class, args);
+	}
+
+}
