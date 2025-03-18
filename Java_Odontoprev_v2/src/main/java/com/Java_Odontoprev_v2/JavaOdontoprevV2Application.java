de4d25b4618f0a6eafde392a46d71ba37e4d1579
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-03-18 16:50:22 -0300

    updade formulario

--- Java_Odontoprev_v2/src/main/java/com/Java_Odontoprev_v2/JavaOdontoprevV2Application.java
+++ Java_Odontoprev_v2/src/main/java/com/Java_Odontoprev_v2/JavaOdontoprevV2Application.java
@@ -1,8 +1,18 @@
 package com.Java_Odontoprev_v2;
 
+import com.Java_Odontoprev_v2.model.Cliente;
+import com.Java_Odontoprev_v2.model.Usuario;
+import com.Java_Odontoprev_v2.repository.ClienteRepository;
+import com.Java_Odontoprev_v2.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
 
 @SpringBootApplication
 @RequiredArgsConstructor
@@ -11,6 +21,5 @@ public class JavaOdontoprevV2Application {
 	public static void main(String[] args) {
 		SpringApplication.run(JavaOdontoprevV2Application.class, args);
 	}
-
 
 }
---
+++ Java_Odontoprev_v2/src/main/java/com/Java_Odontoprev_v2/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.Java_Odontoprev_v2.repository;
+
+import com.Java_Odontoprev_v2.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, String> {
+
+}
