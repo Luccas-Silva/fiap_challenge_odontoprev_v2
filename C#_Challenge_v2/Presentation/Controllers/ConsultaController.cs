b80c55898b17e19b8c9524e9abf1783700719c52
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-03-19 18:55:51 -0300

    project creation and folder structure with clean arch

---
+++ C#_Challenge_v2/Presentation/Controllers/ConsultaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C__Challenge_v2.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConsultaController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Dados retornados com sucesso!");
+        }
+
+    }
+}
---
+++ C#_Challenge_v2/Presentation/Controllers/DentistaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C__Challenge_v2.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DentistaController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Dados retornados com sucesso!");
+        }
+
+    }
+}
