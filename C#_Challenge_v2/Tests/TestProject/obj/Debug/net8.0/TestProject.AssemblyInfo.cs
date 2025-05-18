765d8e2be189fc0784dc1614341d1e15d7a08e77
Luccas Silva <89430801+Luccas-Silva@users.noreply.github.com>
2025-05-18 14:57:01 -0300

    Tests

--- C#_Challenge_v2/Tests/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ C#_Challenge_v2/Tests/TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("C#_Challenge_v2")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8454bdf54b0a756b66780255a25296d4f8a6fa16")]
-[assembly: System.Reflection.AssemblyProductAttribute("C#_Challenge_v2")]
-[assembly: System.Reflection.AssemblyTitleAttribute("C#_Challenge_v2")]
+[assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
+[assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
